4dad431ab7b38cfe98c7b72026c6221deb36c060
Wooter <rick@astley.nevergonnagiveyouup>
2022-04-30 07:07:12 +0200

    Remove need for password

--- skeleton/http/www/cgi-bin/login.sh
+++ skeleton/http/www/cgi-bin/login.sh
@@ -3,6 +3,8 @@
 echo "Content-type: text/html"
 echo ""
 
-tmp=$(echo $QUERY_STRING | sed "s/&/\n/g" | awk 'NR==2{ print; exit }' | sed 's/pass=//g')
+# tmp=$(echo $QUERY_STRING | sed "s/&/\n/g" | awk 'NR==2{ print; exit }' | sed 's/pass=//g')
 
-[ "$(echo $tmp | /bin/md5)" = $(cat /http/priv/password) ] && cat /http/priv/ui.html || echo invaild
+# [ "$(echo $tmp | /bin/md5)" = $(cat /http/priv/password) ] && cat /http/priv/ui.html || echo invaild
+
+cat /http/priv/ui.html
