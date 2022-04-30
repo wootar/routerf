ab51e43f0cb0f1fdd9b5f004fd01a6f7204cd1e7
Wooter <rick@astley.nevergonnagiveyouup>
2022-04-30 10:02:40 +0200

    Clean up, create the filesystem on go

--- build.lua
+++ build.lua
@@ -1,22 +1,45 @@
 #!/bin/lua
 local stages = {}
 
-function mkdir(file)
+local function mkdir(file)
     os.execute("mkdir "..file)
 end
-function touch(file)
+local function copy(file,file2)
+    os.execute("cp -r "..file.." "..file2)
+end
+local function touch(file)
     os.execute("touch "..file)
 end
-function run(script)
+local function run(script)
     os.execute([[sh -c "]]..script..[["]])
 end
 
 stages[1] = function ()
     print("! Creating filesystem")
-    run([[
-        echo TODO
-        exit 0
-    ]])
+    mkdir("rootfs")
+    local filesystem = {
+        "/bin",
+        "/sbin",
+        "/dev",
+        "/sys",
+        "/proc",
+        "/http",
+        "/http/priv",
+        "/http/www",
+        "/etc",
+        "/root"
+    }
+    for _,v in pairs(filesystem) do
+        print("! Creating "..v)
+        mkdir("rootfs"..v)
+    end 
+    print("! Creation of filesystem is done")
+end
+
+stages[2] = function ()
+    print("! Copying the base")
+    copy("base/*","rootfs")
+    print("! Copied the base")
 end
 
 for i,v in pairs(stages) do
